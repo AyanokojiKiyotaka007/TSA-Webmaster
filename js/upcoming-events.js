a6fcffd176803debe5e8d21a4195e00a1c46cb78
swapna <swapna.kotte@gmail.com>
2025-10-29 16:36:13 -0700

    done

--- js/upcoming-events.js
+++ js/upcoming-events.js
@@ -1,6 +1,23 @@
 $(function(){
-
     var $timeline_item = $('.timeline-item');
+    var $timeline_content = $('.timeline-content');
+
+    // Add click handlers to timeline cards
+    $timeline_content.on('click', function() {
+        var $this = $(this);
+        
+        // If already active, close it with animation
+        if ($this.hasClass('active')) {
+            $this.removeClass('active');
+            return;
+        }
+        
+        // Close any other active cards
+        $('.timeline-content.active').removeClass('active');
+        
+        // Open this card
+        $this.addClass('active');
+    });
 
     // Function to check if an element is in viewport
     function isElementInViewport(el) {
@@ -18,12 +35,16 @@ $(function(){
         for (var i = 0; i < $timeline_item.length; i++) {
             if (isElementInViewport($timeline_item[i])) {
                 $($timeline_item[i]).find('.js--fadeInLeft, .js--fadeInRight').addClass('is-visible');
+                // Add a slight delay before showing content
+                setTimeout(function(item) {
+                    $(item).find('.timeline-content').addClass('is-visible');
+                }, 300, $timeline_item[i]);
             }
         }
     }
 
     // Initial check on page load
-    callbackFunc();
+    setTimeout(callbackFunc, 500);
 
     // Listen for scroll event
     $(window).on('scroll', function() {
